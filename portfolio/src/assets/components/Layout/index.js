de92431bf9ac77085c1ef82c162db25615633110
adityadatta11 <adityadatta00@gmail.com>
2024-03-02 23:52:10 +0600

    Added the sidebar, with my logo and a logo subtitle

--- portfolio/src/assets/components/Layout/index.js
+++ portfolio/src/assets/components/Layout/index.js
@@ -1,9 +1,15 @@
-import './index.scss';
-import '../Sidebar';
+import './index.scss'
+import { Link } from 'react-router-dom'
+import LogoA from '../../../assets/images/logo_a.png'
+import LogoSubtitle from '../../../assets/images/logo_sub.png'
 
-// Using some constant layout now
-const Layout = () => {
-    return <>Sidebar</>
-}
+const Sidebar = () => (
+    <div className="nav-bar" to='/'>
+        <Link>
+            <img src={LogoA} alt="Logo Aditya" />
+            <img src={LogoSubtitle} alt="Logo Subtitle Aditya" />
+        </Link>
+    </div>
+)
 
-export default Layout;
+export default Sidebar

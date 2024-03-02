7e809d5f6acfc90878db273f8c73a2460fc197c2
adityadatta11 <adityadatta00@gmail.com>
2024-03-03 00:09:47 +0600

    Added the sidebar, with my logo and a logo subtitle

--- portfolio/src/assets/components/Layout/index.js
+++ portfolio/src/assets/components/Layout/index.js
@@ -5,7 +5,7 @@ import LogoSubtitle from '../../../assets/images/logo_sub.png'
 
 const Sidebar = () => (
     <div className="nav-bar" to='/'>
-        <Link>
+        <Link className="logo" to='/'>
             <img src={LogoA} alt="Logo Aditya" />
             <img className="sub-logo" src={LogoSubtitle} alt="Logo Subtitle Aditya" />
         </Link>
